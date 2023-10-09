ba5b71574553f01f2e2814c25709a799881d129f
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-10-09 19:19:35 -0300

    utilizando o tipo em ts para uma funcao

---
+++ aula-1/index.js
@@ -0,0 +1,6 @@
+console.log('Hi');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(5, 8);
+console.log(result);
---
+++ aula-1/index.ts
@@ -0,0 +1,9 @@
+console.log('Hi')
+
+function add(num1: number, num2: number){
+    return num1 + num2
+}
+
+let result = add(5, 8)
+
+console.log(result)
